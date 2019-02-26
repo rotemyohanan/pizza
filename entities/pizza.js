58e5808df37246aef81082020d907db01c450858
Rotemy <RotemYo@icloud.com>
2019-02-26 22:03:01 +0200

    Update pizza.js

--- entities/pizza.js
+++ entities/pizza.js
@@ -1,1 +1,51 @@
 
+class Pizza {
+
+    constructor(name) {
+        this.name = name
+        this.status = 'PENDING'
+        this.doughTime = ['','']
+        this.toppingTime = ['','']
+        this.ovenTime = ['','']
+        this.waitressTime = ['','']
+        console.log(this)
+    }
+
+}
+
+function setDoughStartTime(pizza, time) {
+    pizza.doughTime[0] = time
+}
+
+function setDoughEndTime(pizza, time) {
+    pizza.doughTime[1] = time
+}
+
+function setToppingStartTime(pizza, time) {
+    pizza.toppingTime[0] = time
+}
+
+function setToppingEndTime(pizza, time) {
+    pizza.toppingTime[1] = time
+}
+
+function setOvenStartTime(pizza, time) {
+    pizza.ovenTime[0] = time
+}
+
+function setOvenEndTime(pizza, time) {
+    pizza.ovenTime[1] = time
+}
+
+function setWaitressStartTime(pizza, time) {
+    pizza.waitressTime[0] = time
+}
+function setWaitressEndTime(pizza, time) {
+    pizza.waitressTime[1] = time
+}
+
+function getorderTime(pizza) {
+    return pizza.doughTime[0] - pizza.waitressTime[1]
+}
+
+module.exports = {Pizza, setDoughStartTime, setDoughEndTime, setToppingEndTime, setToppingStartTime, setOvenStartTime, setOvenEndTime, setWaitressStartTime, setWaitressEndTime, getorderTime}
